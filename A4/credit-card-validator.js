32761eb2fa8645f5e170fd9175132a39a9c32f62
dmitriyk35 <54919299+dmitriyk35@users.noreply.github.com>
2020-05-07 17:26:04 -0400

    Update credit-card-validator.js

--- A4/credit-card-validator.js
+++ A4/credit-card-validator.js
@@ -8,43 +8,29 @@ function validateCreditCard() {
 
 
 
-for (var i = ccNum.length; i>0; i--) {
-  if(((ccNum.length) === 16))
- {
+for (var i = ccNum.length-1; i >= 0; i--) {
+    sum = parseInt(ccNum[i]) + sum;
+    console.log(sum);
+    }
 
-   document.getElementById("demo").innerHTML = "It is a valid credit card.";
- }
- else {
-document.getElementById("demo").innerHTML = "It is NOT a valid credit card.";
-}
-}
-
-
-    for (var i = ccNum.length; i>0; i--) {
-      if((ccNum[0] != ccNum[i]) || (ccNum[2] != ccNum[i]) || (ccNum[3] != ccNum[i]) || (ccNum[4] != ccNum[i]) || (ccNum[5] != ccNum[i]) ||
-     (ccNum[6] != ccNum[i]) || (ccNum[7] != ccNum[i]) || (ccNum[8] != ccNum[i]) || (ccNum[9] != ccNum[i]) ||
-     (ccNum[10] != ccNum[i]) || (ccNum[11] != ccNum[i]) || (ccNum[12] != ccNum[i]) || (ccNum[13] != ccNum[i]) || (ccNum[14] != ccNum[i]) || (ccNum[15] != ccNum[i]))
+for (i = ccNum.length-1; i >= 0; i--) {
+  if(((ccNum.length) < 16) || ((ccNum.length) > 16))
      {
-
-       document.getElementById("demo").innerHTML = "It is a valid credit card.";
+      document.getElementById("demo").innerHTML = "It is NOT a valid credit card. Enter a card with only 16 digits.";
      }
-     else {
-    document.getElementById("demo").innerHTML = "It is NOT a valid credit card. Please enter a 16 digit credit card.";
-  }
-}
-
-for (var i = ccNum.length; i>0; i--) {
-  if(((ccNum[15]%2) === 0))
- {
-
-   document.getElementById("demo").innerHTML = "It is a valid credit card.";
- }
- else {
-document.getElementById("demo").innerHTML = "It is NOT a valid credit card.";
-}
-}
-
-
-
+     else if(sum < 16)
+     {
+      document.getElementById("demo").innerHTML = "It is NOT a valid credit card. <br> Enter a card with entire value greater than 16.";
+     }
+      else if((ccNum[0] === ccNum[i]) && (ccNum[2] === ccNum[i]))
+         {
+          document.getElementById("demo").innerHTML = "It is NOT a valid credit card. <br>Same values for all digits are not allowed.";
+         }
+         else if(((ccNum[15]%2) !== 0))
+     {
+    
+      document.getElementById("demo").innerHTML = "It is NOT a valid credit card. <br>Last digit must be an even number.";
+     }
+    }
 
 }
