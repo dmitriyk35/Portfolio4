200e197e0fa3c7c655e3c74c6d0e8a591b68a6b9
dmitriyk35 <54919299+dmitriyk35@users.noreply.github.com>
2020-05-07 17:48:22 -0400

    Update credit-card-validator.js

--- A4/credit-card-validator.js
+++ A4/credit-card-validator.js
@@ -22,14 +22,17 @@ for (i = ccNum.length-1; i >= 0; i--) {
      {
       document.getElementById("demo").innerHTML = "It is NOT a valid credit card. <br> Enter a card with entire value greater than 16.";
      }
-      else if((ccNum[0] === ccNum[i]) && (ccNum[2] === ccNum[i]))
-         {
-          document.getElementById("demo").innerHTML = "It is NOT a valid credit card. <br>Same values for all digits are not allowed.";
-         }
-         else if(((ccNum[15]%2) !== 0))
+     else if((ccNum[0] === ccNum[i]) && (ccNum[2] === ccNum[i]))
      {
-    
+      document.getElementById("demo").innerHTML = "It is NOT a valid credit card. <br>Same values for all digits are not allowed.";
+     }
+     else if(((ccNum[15]%2) !== 0))
+     {
       document.getElementById("demo").innerHTML = "It is NOT a valid credit card. <br>Last digit must be an even number.";
+     }
+     else
+     {
+      document.getElementById("demo").innerHTML = "It is a valid credit card.";
      }
     }
 
